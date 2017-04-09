19056a4f828518ce6cfff0cb8b84886db80c0e9f
ganieldao <ganieldao@gmail.com>
2017-04-09 06:37:46 -0400

    Add konami code to disable radius :)

--- public/src/GraffitiMap.js
+++ public/src/GraffitiMap.js
@@ -6,6 +6,8 @@ var colorLayerContext;
 var colorLayerWidth;
 var currentPosition;
 
+var godMode = false;
+
 var rectLatLng = new google.maps.LatLng(40, -95);
 var rectWidth = 6.5;
 var radius = .001
@@ -38,6 +40,44 @@ socket.on('broad', function(data) {
     pixels = data;
     update();
 });
+// a key map of allowed keys
+var allowedKeys = {
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  65: 'a',
+  66: 'b'
+};
+
+// the 'official' Konami Code sequence
+var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+
+// a variable to remember the 'position' the user has reached so far.
+var konamiCodePosition = 0;
+
+document.addEventListener('keydown', function(e) {
+  // get the value of the key code from the key map
+  var key = allowedKeys[e.keyCode];
+  // get the value of the required key from the konami code
+  var requiredKey = konamiCode[konamiCodePosition];
+
+  // compare the key with the required key
+  if (key == requiredKey) {
+
+    // move to the next key in the konami code sequence
+    konamiCodePosition++;
+
+    // if the last key is reached, activate cheats
+    if (konamiCodePosition == konamiCode.length)
+      activateCheats();
+  } else
+    konamiCodePosition = 0;
+});
+
+function activateCheats() {
+    godMode = !godMode;
+}
 
 function init() {
     // initialize the map
@@ -187,6 +227,10 @@ function click (e) {
 }
 
 function checkLocation(x, y, callback) {  //Checks the current location of the user
+    if(godMode == true) {
+        callback();
+    }
+
     var myLatLng;           
     var currentIndex;           //Stores the longitude and latitude in point form
     var lat;                    //Current Latitude of user
